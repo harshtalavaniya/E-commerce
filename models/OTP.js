c8377c36be2b67992f02b8484824464c7d631d08
harshtalavaniya <harshtalavaniya8461@gmail.com>
2023-06-12 23:03:17 +0530

    otp preschena created

--- models/OTP.js
+++ models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const mailsender=require("../utils/mailSender")
 
 const OTPSchema= new mongoose.Schema({email: {
     type: String,
@@ -15,3 +16,31 @@ createdAt: {
 },
 
 });
+
+async function sendVerification(email,otp){
+    try{
+        const mailResponse = await mailsender(
+			email,
+			"Verification Email",
+			otp
+		);
+
+    }catch(error){
+        console.log("Error occurred while sending email: ", error);
+		throw error;
+
+    }
+}
+OTPSchema.pre("save", async function (next) {
+	console.log("New document saved to database");
+
+	// Only send an email when a new document is created
+	if (this.isNew) {
+		await sendVerification(this.email, this.otp);
+	}
+	next();
+});
+
+const OTP = mongoose.model("OTP", OTPSchema);
+
+module.exports = OTP;
